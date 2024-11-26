425b4b0499da8e40f5a8f9af37e8f32f27dedfb4
Pratham Jha <pratham.jha2005@gmail.com>
2024-11-26 19:36:53 +0530

    another college shit

---
+++ lxfdebaaaz code's/JAVA Programming/FirstJavaProgram/kotlin.java
@@ -0,0 +1,7 @@
+public class kotlin {
+    public static void main(String[] args) {
+        int n1 = 10, n2 = 20, sum;
+        sum = n1 + n2;
+        System.out.println("Sum of these numbers: "+sum);
+    }
+}
